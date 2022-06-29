190c02d2b171853ce2704024fb6f294253e9797f
Adam Kempler <akempler@gmail.com>
2022-06-29 11:08:38 -0400

    Additional scaffolding.

---
+++ defaults/settings.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Load services definition file.
+ */
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+/**
+ * Include the Pantheon-specific settings file.
+ *
+ * n.b. The settings.pantheon.php file makes some changes
+ *      that affect all environments that this site
+ *      exists in.  Always include this file, even in
+ *      a local development environment, to ensure that
+ *      the site settings remain consistent.
+ */
+include __DIR__ . "/settings.pantheon.php";
+
+// $settings['config_sync_directory'] = dirname(DRUPAL_ROOT) . '/config/sync';
+$settings['config_sync_directory'] = dirname(DRUPAL_ROOT) . '/config';
+
+/**
+ * Skipping permissions hardening will make scaffolding
+ * work better, but will also raise a warning when you
+ * install Drupal.
+ *
+ * https://www.drupal.org/project/drupal/issues/3091285
+ */
+// $settings['skip_permissions_hardening'] = TRUE;
+
+/**
+ * If there is a local settings file, then include it
+ */
+$local_settings = __DIR__ . "/settings.local.php";
+if (file_exists($local_settings)) {
+  include $local_settings;
+}
+
+if (file_exists(__DIR__ . '/settings.lando.php')) {
+    include __DIR__ . '/settings.lando.php';
+}
